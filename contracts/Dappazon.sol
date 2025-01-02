baf3e52a8317508112f6c885e101360400fbf571
Akash <akash2904r@gmail.com>
2025-01-02 20:35:50 +0530

    Add buy products functionality

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -14,9 +14,17 @@ contract Dappazon {
         uint256 stock;
     }
 
+    struct Order {
+        uint256 time;
+        Item item;
+    }
+
     mapping(uint256 => Item) public items;
+    mapping(address => uint256) public orderCount;
+    mapping(address => mapping(uint256 => Order)) public orders;
 
     event List(string name, uint256 cost, uint256 quantity);
+    event Buy(address buyer, uint256 orderId, uint256 itemId);
 
     modifier onlyOwner() {
         require(msg.sender == owner, "You are not the owner");
@@ -45,5 +53,29 @@ contract Dappazon {
         items[_id] = item;
         // Emit the list event
         emit List(_name, _cost, _stock);
+    }
+
+    // Buy the products
+    function buy(uint256 _id) public payable {
+        // Fetch the item
+        Item memory item = items[_id];
+
+        // Check whether the specified item is in stock
+        require(item.stock > 0, "Specified item is out of stock");
+
+        // Check whether enough ether is sent to buy the item
+        require(msg.value >= item.cost, "Amount sent isn't sufficient to buy the specified product");
+
+        // Create an order
+        Order memory order = Order(block.timestamp, item);
+
+        // Save order to chain
+        orderCount[msg.sender]++;
+        orders[msg.sender][orderCount[msg.sender]] = order;
+
+        // Update the stock
+        items[_id].stock = item.stock - 1;
+        // Emit the Buy event
+        emit Buy(msg.sender, orderCount[msg.sender], item.id);
     }
 }
