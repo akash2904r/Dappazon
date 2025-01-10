c90ce837a8a5b1f2cb3f570d4ff8138024c09156
Akash <akash2904r@gmail.com>
2025-01-10 21:44:05 +0530

    Complete Section component

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,12 +1,38 @@
-import { ethers } from 'ethers'
+import { ethers } from 'ethers';
 
-// Components
-import Rating from './Rating'
+import Rating from './Rating';
+import { parseETH } from '../utils';
 
 const Section = ({ title, items, togglePop }) => {
     return (
-        <div className='cards__section'>
+        <div className="cards__section">
+            <h3 id={title}>
+                {title}
+            </h3>
 
+            <hr />
+
+            <div className="cards">
+                {items.map((item, index) => (
+                    <div
+                        key={index}
+                        className="card"
+                        onClick={() => togglePop(item)}
+                    >
+                        <div className="card__image">
+                            <img 
+                                src={item.image} 
+                                alt={item.name} 
+                            />
+                        </div>
+                        <div className="card__info">
+                            <h4>{item.name}</h4>
+                            <Rating value={item.rating} />
+                            <p>{parseETH(item.cost)} ETH</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
